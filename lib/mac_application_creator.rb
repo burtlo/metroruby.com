7a1d0839fc1a578d1eb9dd308d3939dfdb70f3be
Franklin Webber <franklin.webber@gmail.com>
2012-11-23 16:26:51 -0500

    Changed target_archive to target

--- lib/mac_application_creator.rb
+++ lib/mac_application_creator.rb
@@ -11,20 +11,20 @@ module Metro
       url
     end
 
-    attr_reader :target_archive, :source
+    attr_reader :target, :source
 
     def initialize(source,target)
       @source = source
-      @target_archive = target
+      @target = target
     end
 
     def create!
-      FileUtils.cp_r application_wrapper_path, target_archive
+      FileUtils.cp_r application_wrapper_path, target
       FileUtils.cp_r Dir["#{source}/**/*"], source_target_path
     end
 
     def source_target_path
-      File.join target_archive, *path_within_app
+      File.join target, *path_within_app
     end
 
     def path_within_app
@@ -34,19 +34,19 @@ module Metro
     def application_wrapper_path
       File.absolute_path File.join File.dirname(__FILE__), "..", "public", "Game.app"
     end
-    
+
     def archive!
       old_working_directory = Dir.pwd
       Dir.chdir File.dirname(target_tar_archive)
       tgz = Zlib::GzipWriter.new(File.open(target_tar_archive, 'wb'))
-      Archive::Tar::Minitar.pack(File.basename(target_archive), tgz)
+      Archive::Tar::Minitar.pack(File.basename(target), tgz)
       Dir.chdir old_working_directory
     end
-    
+
     def target_tar_archive
-      File.join File.dirname(target_archive), 'mac.tar.gz'
+      File.join File.dirname(target), 'mac.tar.gz'
     end
-    
+
     def upload_to_s3!
       s3 = AWS::S3.new
       bucket = s3.buckets['rubymetro-dev']
@@ -57,15 +57,15 @@ module Metro
     end
 
     def amazon_folder
-      File.dirname(target_archive)[/\/([^\/]+\/[^\/]+)$/,1]
+      File.dirname(target)[/\/([^\/]+\/[^\/]+)$/,1]
     end
-    
+
     def amazon_archive
       File.join amazon_folder, File.basename(target_tar_archive)
     end
-    
+
     def clean!
-      FileUtils.rm_rf target_archive
+      FileUtils.rm_rf target
       FileUtils.rm_rf target_tar_archive
     end
 
